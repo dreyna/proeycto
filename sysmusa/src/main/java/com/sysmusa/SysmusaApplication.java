933407261aaf5248a5f9a0490719acaecb52e0c1
Administrador <reyna_b@upeu.edu.pe>
2019-10-17 07:46:27 -0500

    v1

---
+++ sysmusa/src/main/java/com/sysmusa/SysmusaApplication.java
@@ -0,0 +1,13 @@
+package com.sysmusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysmusaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysmusaApplication.class, args);
+	}
+
+}
